34413cda110255b98433e516f3a90a4e2a77faeb
yuanchengwang <1057041679@qq.com>
2022-10-12 02:01:34 -0400

    add result

--- project1_template/run_p.sh
+++ project1_template/run_p.sh
@@ -1,3 +1,3 @@
 mpic++ odd_even_parallel_sort.cpp -o psort
-mpirun -np 32 ./psort 131072 ./test_data/131072a.in
-./check 131072 ./test_data/131072a.in.parallel.out
+mpirun -np 4 ./psort 20 ./test_data/20a.in
+./check 20 ./test_data/20a.in.parallel.out
--- project1_template/run_s.sh
+++ project1_template/run_s.sh
@@ -1,3 +1,3 @@
 g++ odd_even_sequential_sort.cpp -o ssort
-./ssort 131072 ./test_data/131072a.in
-./check 131072 ./test_data/131072a.in.seq.out
+./ssort 20 ./test_data/20a.in
+./check 20 ./test_data/20a.in.seq.out
